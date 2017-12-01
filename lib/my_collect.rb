932616dbdf3052b4be2f34be5f74af20e9cde6e1
James Ferguson <ncjamesferguson@gmail.com>
2017-12-01 06:27:12 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,15 +1,10 @@
 def my_collect(array)
-
-  if block_given?
-
-    i = 0
+  i = 0
     collection = []
     while i < array.length
       collection << yield(array[i])
       i += 1
     end
-
-  end
   array
 end
 
